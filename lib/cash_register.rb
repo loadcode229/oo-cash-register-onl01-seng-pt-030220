1e756609344f3bee3ed827d8e23145c57303dd02
Adam Priest <sepone229@gmail.com>
2020-03-28 22:39:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,8 +13,8 @@ class CashRegister
   #end
   
   def add_item(item, price, quantity = 1)
-    @price = price
     @total += price * quantity
+    @transactions = 
     if quantity > 1
       counter = 0
       while counter < quantity
